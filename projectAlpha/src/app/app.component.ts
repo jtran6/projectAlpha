6bba47f0b5ecc813bb9fdb52cba057629bd9df92
jtran6 <josh.tran007@gmail.com>
2020-05-15 00:17:54 -0400

    Starting to implement side menu navigation

--- projectAlpha/src/app/app.component.ts
+++ projectAlpha/src/app/app.component.ts
@@ -10,11 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  navigate : any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();
     this.initializeApp();
   }
 
@@ -23,5 +25,26 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  sideMenu()
+  {
+    this.navigate =
+    [
+      {
+        title : "Home",
+        url   : "/tab1",
+        icon  : "home-outline"
+      },
+      {
+        title : "Tab 2",
+        url   : "/tab2",
+        icon  : "list-outline"
+      },
+      {
+        title : "About",
+        url   : "/tab3",
+        icon  : "information-circle-outline"
+      },
+    ]
   }
 }
